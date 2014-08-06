bf1051795b504b391b9075f07fc90f3dd1ab2387
Matt Thees <mthees7@gmail.com>
2014-08-06 19:14:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiReadApp::Application.config.secret_key_base = 'ef7e3e1d2e06c0897a5fd3c99ece5c5d62b1e63ce3317ecfaa23a9bfcfbcfeb94453ae32548f1021cd79f905e800eaa1e0c9405955bf173bad56850c9e6f84c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MultiReadApp::Application.config.session_store :cookie_store, key: '_multi_read_app_session'
